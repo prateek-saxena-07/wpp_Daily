4449df5eedb84cd73507a9915a6e6784c01b6f91
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-08-14 08:56:18 +0530

    wpp26

---
+++ wpp26/wpp26.js
@@ -0,0 +1,16 @@
+function copyToClipboard(text) {
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        console.log("Text copied to clipboard");
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  
+  }
+}
+
+// Usage example
+copyToClipboard("Hello, World!");
